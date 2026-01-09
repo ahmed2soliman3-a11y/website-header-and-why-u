560c1c47c5c7d10b0c4676efc2087745d8ad8c80
Ahmed Ali <ahmed2soliman3@gmail.com>
2026-01-09 15:32:39 +0200

    Adjust hero image position on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export function Header() {
             alt="رونق - معطرات سيارات"
             width={100}
             height={50}
-            className="w-20 sm:w-24 h-auto -mt-5 -ml-6"
+            className="w-20 sm:w-24 h-auto -mt-4 -ml-6"
           />
         </Link>
       </div>
